97fa566047d01ab22c76af324160cbf64701280b
e.rose <e.rose@student.isartdigital.com>
2025-05-23 10:54:49 +0200

    [ADD] add macro to compile shared / static lib with window / linux

--- Code/Include/Utils/Export.hpp
+++ Code/Include/Utils/Export.hpp
@@ -1,3 +1,25 @@
 #pragma once
 
-// TODO : Add macro for export (dllspec for window and whatever other platform use)
+// Case : Platforms : Window / Linux, Build : shared or static
+
+#if defined(_DEBUG) || defined(DEBUG) || defined(__DEBUG__) || defined(__DEBUG)
+	#ifndef KRHI_DEBUG
+		#define KRHI_DEBUG
+	#endif
+#else 
+	#define KRHI_RELEASE 
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+	#if defined(KRHI_STATIC_DEFINE)
+		#define KRHI_API
+	#else
+		#define KRHI_API __declspec(dllexport)
+#endif
+#else
+	#if defined(KRHI_STATIC_DEFINE)
+		#define KRHI_API
+	#else
+		#define KRHI_API __attribute__((visibility("default")))
+	#endif
+#endif
