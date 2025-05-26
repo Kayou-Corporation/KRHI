c65760ae23c8743b5d76a777d793cb867f40aedd
e.rose <e.rose@student.isartdigital.com>
2025-05-26 15:12:19 +0200

    [TEST] test with glfw and basic instance & debug messenger

--- Test/Renderer/Main.cpp
+++ Test/Renderer/Main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include "Platforms/Vulkan/VulkanInterface.hpp"
+
 
 using namespace KRHI;
 
@@ -11,6 +15,15 @@ int main()
     
     Ref<Common::Instance> instance = interface->InstantiateInstance();
 
-    //instance->Create()
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+    instance->Create(extensions, { Common::ValidationLayers::Validation,Common::ValidationLayers::Debug, Common::ValidationLayers::Monitor }, "KRHI", { 0,0,1 }, "KRHI", { 0,0,1 });
+
+    delete glfwExtensions;
+    extensions.clear();
+
+    instance->Destroy();
     return 0;
 }
