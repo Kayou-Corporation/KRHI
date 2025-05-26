4431241375a93ef24758d465ed226bff6375083b
e.rose <e.rose@student.isartdigital.com>
2025-05-26 09:14:03 +0200

    [DONE] Done instance class & override vulkan. Need to check if extensions & layers are completely copy & can be clear (they are for now)

--- Code/Source/Platforms/Vulkan/VulkanInstance.cpp
+++ Code/Source/Platforms/Vulkan/VulkanInstance.cpp
@@ -4,39 +4,39 @@ namespace KRHI::Vulkan
 {
 	void VulkanInstance::Create(std::vector<const char*> extensions, const std::vector<Common::ValidationLayers>& layers, const std::string& appName, const Common::Version& appVersion, const std::string& engineName, const Common::Version& engineVersion)
 	{
-		//VkApplicationInfo appInfo
-		//{
-		//	.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
-		//	.pApplicationName = appName.c_str(),
-		//	.applicationVersion = VK_MAKE_VERSION(appVersion.major, appVersion.minor, appVersion.patch),
-		//	.pEngineName = engineName.c_str(),
-		//	.engineVersion = VK_MAKE_VERSION(engineVersion.major, engineVersion.minor, engineVersion.patch),
-		//	.apiVersion = VK_API_VERSION_1_4
-		//};
-		//
-		//std::vector<const char*> vulkanLayers = GetLayers(layers);
-		//VkInstanceCreateInfo createInfo
-		//{
-		//	.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
-		//	.pApplicationInfo = &appInfo,
-		//	.enabledExtensionCount = static_cast<uint32_t>(extensions.size()),
-		//	.ppEnabledExtensionNames = extensions.data(),
-		//
-		//	#ifdef KRHI_DEBUG
-		//	.enabledLayerCount = vulkanLayers.size(),
-		//	.ppEnabledLayerNames = vulkanLayers.data()
-		//	#else
-		//	.enabledLayerCount = 0,
-		//	.ppEnabledLayerNames = {}
-		//	#endif
-		//};
 
+		vk::ApplicationInfo appInfo{};
+		appInfo.pApplicationName = appName.c_str();
+		appInfo.applicationVersion = VK_MAKE_VERSION(appVersion.major, appVersion.minor, appVersion.patch);
+		appInfo.pEngineName = engineName.c_str();
+		appInfo.engineVersion = VK_MAKE_VERSION(engineVersion.major, engineVersion.minor, engineVersion.patch);
+		appInfo.apiVersion = VK_API_VERSION_1_4;
+		
+		std::vector<const char*> vulkanLayers = GetLayers(layers);
 
+		vk::InstanceCreateInfo createInfo;
+		createInfo.pApplicationInfo = &appInfo;
+		createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+		createInfo.ppEnabledExtensionNames = extensions.data();
+
+#ifdef KRHI_DEBUG
+		createInfo.enabledLayerCount = static_cast<uint32_t>(vulkanLayers.size());
+		createInfo.ppEnabledLayerNames = vulkanLayers.data();
+#else
+		createInfo.enabledLayerCount = 0;
+		createInfo.ppEnabledLayerNames = {};
+#endif
+
+		m_handle = vk::createInstance(createInfo);
+		m_extensions = extensions;
+
+		vulkanLayers.clear();
+		extensions.clear();
 	}
 
 	void VulkanInstance::Destroy()
 	{
-		vkDestroyInstance(m_handle, nullptr);
+		m_handle.destroy();
 	}
 
 	std::vector<const char*> VulkanInstance::GetLayers(const std::vector<Common::ValidationLayers>& abstractLayers)
